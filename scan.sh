296fbfa5d5e3c6b6c7e239bf2197cbe8d675fb20
Michael Dick <mi-dick@gmx.de>
2017-12-05 21:31:27 +0100

    all.sh macht alles: Scannt und spuckt ein txt aus

--- scan.sh
+++ scan.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sudo scanimage --resolution 900 --mode Gray --format=pnm > scan.pnm
 #sudo scanimage --resolution 900 --mode Gray --format tiff > document.tiff
-sudo scanimage --format=pnm --mode Gray --resolution 300 --brightness -20 --contrast 15 > scan.pnm
+#sudo scanimage --format=pnm --mode Gray --resolution 300 --brightness -20 --contrast 15 > scan.pnm
 
